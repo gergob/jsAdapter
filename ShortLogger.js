f1791ccbc4da982bdb047e2b5b4aa2fca4ad7687
Gergo Bogdan <bogdangergo@gmail.com>
2016-11-14 07:40:39 +0100

    beatified code

--- ShortLogger.js
+++ ShortLogger.js
@@ -1,21 +1,21 @@
 function ShortLogger(name) {
     var LOG_HEADER = '[' + name + ']';
     return {
-      i: function i(message) {
-        console.info(LOG_HEADER + '[INFO]: ' + message);
-      },
-      d: function d(message) {
-        console.debug(LOG_HEADER + '[DEBG]: ' + message);
-      },
-      w: function w(message) {
-        console.warn(LOG_HEADER + '[WARN]: ' + message);
-      },
-      e: function e(message) {
-        console.error(LOG_HEADER + '[ERRO]:' + message);
-      }
+        i: function i(message) {
+            console.info(LOG_HEADER + '[INFO]: ' + message);
+        },
+        d: function d(message) {
+            console.debug(LOG_HEADER + '[DEBG]: ' + message);
+        },
+        w: function w(message) {
+            console.warn(LOG_HEADER + '[WARN]: ' + message);
+        },
+        e: function e(message) {
+            console.error(LOG_HEADER + '[ERRO]:' + message);
+        }
     }
 }
 
 module.exports = {
-  getLogger: ShortLogger
+    getLogger: ShortLogger
 }
