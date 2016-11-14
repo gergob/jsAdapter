80469f8fcb4440429e41467d9f3b840226f5a907
Gergo Bogdan <bogdangergo@gmail.com>
2016-11-14 07:40:08 +0100

    added short logger implementation

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Adaptern pattern here.');
+var ShortLogger = require('./ShortLogger');
+
+var shortLog = ShortLogger.getLogger('TestLoger');
+
+shortLog.i('This is an information logged using an instance of short logger.');
+shortLog.w('This is a warining logged using instance of short logger.');
