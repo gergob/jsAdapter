cead908d9e1838e8d7ea377b5083504107a2cb80
Gergo Bogdan <bogdangergo@gmail.com>
2016-11-23 22:05:08 +0100

    refactored adapter

--- LoggerAdapter.js
+++ LoggerAdapter.js
@@ -15,96 +15,56 @@ function LoggerAdapter(loggerObj) {
         BAD_LOGGER: 'BadLogger',
         SHORT_LOGGER: 'ShortLogger'
     };
+    var loggerFunctionMapper = {};
+
+    if(loggerObj.getType() === CONSTANTS.BAD_LOGGER) {
+        loggerFunctionMapper[CONSTANTS.DEBUG] = loggerObj.debg;
+        loggerFunctionMapper[CONSTANTS.INFORMATION] = loggerObj.information;
+        loggerFunctionMapper[CONSTANTS.WARNING] = loggerObj.w;
+        loggerFunctionMapper[CONSTANTS.ERROR] = loggerObj.err;
+    }
+    else if (loggerObj.getType() === CONSTANTS.SHORT_LOGGER) {
+        loggerFunctionMapper[CONSTANTS.DEBUG] = loggerObj.d;
+        loggerFunctionMapper[CONSTANTS.INFORMATION] = loggerObj.i;
+        loggerFunctionMapper[CONSTANTS.WARNING] = loggerObj.w;
+        loggerFunctionMapper[CONSTANTS.ERROR] = loggerObj.e;
+    }
 
     var information = function information(message) {
-        switch (loggerObj.getType()) {
-            case CONSTANTS.BAD_LOGGER:
-                logWithBadLogger(CONSTANTS.INFORMATION, message);
-                break;
-            case CONSTANTS.SHORT_LOGGER:
-                logWithShortLogger(CONSTANTS.INFORMATION, message);
-                break;
-            default:
-                throw Error('No implementation for Logger: ' + loggerObj.toString());
+        try {
+          loggerFunctionMapper[CONSTANTS.INFORMATION](message);
+        }
+        catch(err) {
+            throw Error('No implementation for Logger: ' + loggerObj.toString());
         }
     };
 
     var debug = function debug(message) {
-        switch (loggerObj.getType()) {
-            case CONSTANTS.BAD_LOGGER:
-                logWithBadLogger(CONSTANTS.DEBUG, message);
-                break;
-            case CONSTANTS.SHORT_LOGGER:
-                logWithShortLogger(CONSTANTS.DEBUG, message);
-                break;
-            default:
-                throw Error('No implementation for Logger: ' + loggerObj.toString());
+        try {
+          loggerFunctionMapper[CONSTANTS.DEBUG](message);
+        }
+        catch(err) {
+            throw Error('No implementation for Logger: ' + loggerObj.toString());
         }
     };
 
     var warning = function warning(message) {
-        switch (loggerObj.getType()) {
-            case CONSTANTS.BAD_LOGGER:
-                logWithBadLogger(CONSTANTS.WARNING, message);
-                break;
-            case CONSTANTS.SHORT_LOGGER:
-                logWithShortLogger(CONSTANTS.WARNING, message);
-                break;
-            default:
-                throw Error('No implementation for Logger: ' + loggerObj.toString());
+        try {
+          loggerFunctionMapper[CONSTANTS.WARNING](message);
+        }
+        catch(err) {
+            throw Error('No implementation for Logger: ' + loggerObj.toString());
         }
     };
 
     var error = function error(message) {
-        switch (loggerObj.getType()) {
-            case CONSTANTS.BAD_LOGGER:
-                logWithBadLogger(CONSTANTS.ERROR, message);
-                break;
-            case CONSTANTS.SHORT_LOGGER:
-                logWithShortLogger(CONSTANTS.ERROR, message);
-                break;
-            default:
-                throw Error('No implementation for Logger: ' + loggerObj.toString());
+        try {
+          loggerFunctionMapper[CONSTANTS.ERROR](message);
+        }
+        catch(err) {
+            throw Error('No implementation for Logger: ' + loggerObj.toString());
         }
     };
-
-    var logWithBadLogger = function logWithBadLogger(logEntryType, message) {
-        switch (logEntryType) {
-            case CONSTANTS.DEBUG:
-                loggerObj.debg(message);
-                break;
-            case CONSTANTS.INFORMATION:
-                loggerObj.information(message);
-                break;
-            case CONSTANTS.WARNING:
-                loggerObj.w(message);
-                break;
-            case CONSTANTS.ERROR:
-                loggerObj.err(message);
-                break;
-            default:
-                throw Error('Log Entry type NOT AVAILABLE, please use one of the following:' + JSON.stringify(CONSTANTS));
-        }
-    };
-
-    var logWithShortLogger = function logWithShortLogger(logEntryType, message) {
-        switch (logEntryType) {
-            case CONSTANTS.DEBUG:
-                loggerObj.d(message);
-                break;
-            case CONSTANTS.INFORMATION:
-                loggerObj.i(message);
-                break;
-            case CONSTANTS.WARNING:
-                loggerObj.w(message);
-                break;
-            case CONSTANTS.ERROR:
-                loggerObj.e(message);
-                break;
-            default:
-                throw Error('Log Entry type NOT AVAILABLE, please use one of the following:' + JSON.stringify(CONSTANTS));
-        }
-    }
 
     return {
         debug: debug,
